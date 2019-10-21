d4bdc7efebb776767287f31fd60c968fb85fea3a
Fede Jinich <federico@iovlabs.org>
2019-10-21 02:20:42 -0300

    :sparkles: sending rbtc through api

--- pages/api/dispense.ts
+++ pages/api/dispense.ts
@@ -1,21 +1,36 @@
 import Store from '../../store/root-store';
 import { NextApiRequest, NextApiResponse } from 'next';
 import FaucetUtil from '../../utils/faucet-util';
+import { PromiEvent, TransactionReceipt } from 'web3-core';
+import Web3Util from '../../utils/web3-util';
+import config from '../../config.json';
 
-const handleDispense = (req: NextApiRequest, res: NextApiResponse): void => {
+const faucetUtil = new FaucetUtil();
+const web3Util = new Web3Util();
+
+const handleDispense = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     res.setHeader('Content-Type', 'application/json');
 
     const address: string = req.body.address;
+    let faucetNonce: number = -1;
 
+    try{
+        faucetNonce = await web3Util.nonceForAddress(config.FAUCET_ADDRESS);
+        console.log('este nonce' + faucetNonce);
+    } catch(e) {
+        console.error(e);
+        res.status(500).end({});
+    }
+    
     if(!Store.getInstance().alreadyDispensed(address)) {
-        new FaucetUtil().dispense(address)
+        faucetUtil.dispense(address, faucetNonce)
             .on('transactionHash', (txHash: string) => {
                 Store.getInstance().saveDispensedAddress(address);
+                res.setHeader('Content-Type', 'application/json');
                 res.status(200).json({ transactionHash: txHash });
-            })
-            .on('error', (e: Error) => res.status(400).json({ error: e }))
+            }).on('error', (e: Error) => res.status(400).json({ error: e }));     
     } else {
-        res.status(204).end() //if its already dispensed
+        res.status(204).end({}) //if its already dispensed
     }
 }
 
--- test/api.spec.js
+++ test/api.spec.js
@@ -1,14 +1,19 @@
 const assert = require('chai').assert;
 const axios = require('axios');
 
+const TEST_ADDRESS = require('./../config').TEST_ADDRESS;
 const API_URL = 'http://localhost:3000/api';
 
-describe('API TEST', () => {
+describe('Faucet API', () => {
     describe('/dispense', () => {
-        describe('# sending a request', () =>
-            it('> should respond', async () => {
-                const res = await axios.post(API_URL + '/dispense', {address: ''})
-                assert.ok(res.status == 200 || res.status == 400 || res.status == 204)
+        describe('# dispensing to a new address', () =>
+            it('> should respond 200 and a txHash', async () => {
+                const endPoint = API_URL + '/dispense';
+
+                const res = await axios.post(endPoint, {
+                    address: TEST_ADDRESS
+                })
+                assert.ok(res.status == 200);  
             })
         )
     })    
