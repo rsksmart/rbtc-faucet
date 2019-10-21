6c84cd21e117cc7bc0aedabdf6f476d034455921
Fede Jinich <federico@iovlabs.org>
2019-10-21 14:36:41 -0300

    :art: logging like a pro

--- pages/api/dispense.ts
+++ pages/api/dispense.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import config from '../../config.json';
 import Tx from 'ethereumjs-tx';
 import Web3 from 'web3';
-import chalk from 'chalk';
+import logger from './../../utils/logger';
 
 type txParameters = {
     from: string,
@@ -16,8 +16,6 @@ type txParameters = {
 interface FaucetHistory { [address: string]: string; }
 let faucetHistory: FaucetHistory  = {}
 
-const logInfo = (info: string): void => logInfo('[ ' + chalk.cyan.dim('info') + ' ] ' + info);
-
 const handleDispense = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     res.setHeader('Content-Type', 'application/json');
 
@@ -27,7 +25,7 @@ const handleDispense = async (req: NextApiRequest, res: NextApiResponse): Promis
     const dispenseAddress: string = req.body.address;
     
     if(!faucetHistory.hasOwnProperty(dispenseAddress)) {
-        console.log('dispensing to ' + dispenseAddress);
+        logger.event('dispensing to ' + dispenseAddress);
         
         const txParameters: txParameters = {
             from: config.FAUCET_ADDRESS,
@@ -38,35 +36,35 @@ const handleDispense = async (req: NextApiRequest, res: NextApiResponse): Promis
             value: web3.utils.toHex(Number(web3.utils.toWei(config.VALUE_TO_DISPENSE.toString()))),            
         }
 
-        console.log('from ' + txParameters.from);
-        console.log('to ' + txParameters.to);
-        console.log('nonce ' + txParameters.nonce);
-        console.log('gasPrice ' + txParameters.gasPrice);
-        console.log('gas ' + txParameters.gas);
-        console.log('value ' + txParameters.value);
+        logger.info('from ' + txParameters.from);
+        logger.info('to ' + txParameters.to);
+        logger.info('nonce ' + txParameters.nonce);
+        logger.info('gasPrice ' + txParameters.gasPrice);
+        logger.info('gas ' + txParameters.gas);
+        logger.info('value ' + txParameters.value);
 
         let tx = new Tx(txParameters);
         tx.sign(Buffer.from(config.FAUCET_PRIVATE_KEY, 'hex'));
         const encodedTx = '0x' + tx.serialize().toString('hex');
 
-        console.log('encodedTx ' + encodedTx);
+        logger.info('encodedTx ' + encodedTx);
         
         web3.eth.sendSignedTransaction(encodedTx)
             .on('transactionHash', (txHash: string) => {
-                console.log('dispensed to ' + dispenseAddress);
-                console.log('tx hash ' + txHash);
+                logger.success('dispensed to ' + dispenseAddress);
+                logger.success('tx hash ' + txHash);
 
                 faucetHistory[dispenseAddress] = 'dispensed';
                 res.setHeader('Content-Type', 'application/json');
                 res.status(200).json({ transactionHash: txHash });
             })
             .on('error', (e: Error) => { 
-                console.error('something went wrong');
-                console.error(e);
+                logger.error('something went wrong');
+                logger.error(e);
                 res.status(400).json({ error: e }) 
             });     
     } else {
-        console.log('this address has reached dispensing limit');
+        logger.warning('this address has reached dispensing limit');
         res.status(204).end()
     }
 }
