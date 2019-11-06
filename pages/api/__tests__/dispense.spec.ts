152ec79b4a822ad7b2d6b4c4779e125e7f89da80
Fede Jinich <federico@iovlabs.org>
2019-11-06 12:44:22 -0300

    :sparkles: testing at the right way

--- pages/api/__tests__/dispense.spec.ts
+++ pages/api/__tests__/dispense.spec.ts
@@ -3,26 +3,26 @@ import Web3 from 'web3';
 import http from 'http';
 import listen from 'test-listen';
 import { apiResolver } from 'next-server/dist/server/api-utils';
-import sinon from 'sinon';
 import { captchaApiUrl, valueToDispense, provider } from '../../../utils/env-util';
 import handleDispense from '../dispense';
+import { isValidAddress, isValidChecksumAddress } from 'rskjs-util';
 
 const CAPTCHA_API_URL = captchaApiUrl();
 const VALUE_TO_DISPENSE = valueToDispense();
+const TESTNET_CHAIN_ID = 31;
 
-//let clock = sinon.useFakeTimers();
-var ganacheServer;
 var web3: Web3;
 var server: http.Server;
 var accounts: string[];
 var apiUrl: string;
+var faucetAddress: string;
 
 beforeAll(async () => {
   web3 = new Web3(provider());
   web3.transactionConfirmationBlocks = 1;
+  web3.eth.transactionConfirmationBlocks = 1;
   accounts = await web3.eth.getAccounts();
-
-  console.log('this accs: ' + JSON.stringify(accounts));
+  faucetAddress = accounts[0];
 
   let requestHandler = (req: any, res: any) => {
     return apiResolver(req, res, undefined, handleDispense);
@@ -34,9 +34,7 @@ beforeAll(async () => {
   setupRNS();
 });
 
-afterEach(() => resetFaucetHistory());
-
-afterAll(() => {
+afterAll(async () => {
   //clock.restore();
   server.close();
 });
@@ -44,37 +42,37 @@ afterAll(() => {
 test('there are four accounts', () => {
   expect.assertions(2);
 
-  const prefixArray = accounts.map(e => e.substring(0, 2));
+  const validAddresses = accounts.map(e => isValidAddress(e));
 
   expect(accounts.length).toBe(4);
-  expect(prefixArray.every(e => e == '0x')).toBeTruthy();
+  expect(validAddresses.every(e => e)).toBeTruthy();
 });
 
-test('dispense to a new address, should respond 200 and a txHash', async () => {
+test('dispense to a new address', async () => {
   expect.assertions(3);
 
   const response = await axios.post(apiUrl, { dispenseAddress: accounts[1] });
-  const balance = await web3.eth.getBalance(accounts[1]);
+  const balance = Number(web3.utils.fromWei(await web3.eth.getBalance(accounts[1]), 'ether'));
 
   expect(response.status).toBe(200);
   expect(response.data.txHash).toBeTruthy();
-  expect(balance).toBe(100000000000000000);
+  expect(balance).toBe(valueToDispense());
 });
 
 test('# dispense more than one time to an address, should only dispense the first time and then deny', async () => {
   expect.assertions(5);
 
   const dispenseAddress = accounts[2];
-  const oldBalance = await web3.eth.getBalance(dispenseAddress);
+  const oldBalance = web3.utils.fromWei(await web3.eth.getBalance(dispenseAddress), 'ether');
   const firstResponse = await axios.post(apiUrl, {
     dispenseAddress,
-    captcha: 'asd'
+    captcha: 'NO_CAPTCHA'
   });
 
   try {
     await axios.post(apiUrl, {
       dispenseAddress,
-      captcha: 'asd'
+      captcha: 'NO_CAPTCHA'
     });
   } catch (e) {
     expect(e.response.status).toBe(409);
@@ -83,7 +81,7 @@ test('# dispense more than one time to an address, should only dispense the firs
   try {
     await axios.post(apiUrl, {
       dispenseAddress,
-      captcha: 'asd'
+      captcha: 'NO_CAPTCHA'
     });
   } catch (e) {
     expect(e.response.status).toBe(409);
@@ -92,56 +90,67 @@ test('# dispense more than one time to an address, should only dispense the firs
   expect(firstResponse.status).toBe(200);
   expect(firstResponse.data.txHash).toBeTruthy();
 
-  const currentBalance = await web3.eth.getBalance(dispenseAddress);
-  const expectedBalance = incrementByValueToDispense(oldBalance);
+  const currentBalance = Number(web3.utils.fromWei(await web3.eth.getBalance(dispenseAddress), 'ether'));
+  const expectedBalance = Number(oldBalance) + Number(valueToDispense());
 
-  expect(Number(currentBalance)).toBe(expectedBalance);
+  expect(currentBalance).toBe(expectedBalance);
 });
 
-/*
+
 test("# dispense to an invalid address, shouldn't dispense and respond 409", async () => {
+  expect.assertions(2);
+
+  const oldFaucetTransactionCount = await web3.eth.getTransactionCount(faucetAddress);
+
   try {
-    await axios.post(API_URL + '/dispense', {
-      dispenseAddress: '0x0'
+    await axios.post(apiUrl, {
+      dispenseAddress: '0x0',
+      captcha: "NO_CAPTCHA",
     });
   } catch (e) {
+    const newFaucetTransactionCount = await web3.eth.getTransactionCount(faucetAddress);
     expect(e.response.status).toBe(409);
+    expect(newFaucetTransactionCount).toBe(oldFaucetTransactionCount);
   }
 });
-test('# dispense to an invalid checksum address (TODO), should dispense and respond 200', async () => {
-  const res = await axios.post(API_URL + '/dispense', {
-    dispenseAddress: '0xD5e6Bfc7E5d982C1F7bAc4D9c7e769BEb0A6f626',
+test('# dispense to an invalid checksum address, should dispense and respond 200', async () => {
+  expect.assertions(4);
+  
+  const unchecksumedAddress: string = '0xD5e6Bfc7E5d982C1F7bAc4D9c7e769BEb0A6f626';
+
+  expect(isValidChecksumAddress(unchecksumedAddress, TESTNET_CHAIN_ID)).toBeFalsy();
+
+  const res = await axios.post(apiUrl, {
+    dispenseAddress: unchecksumedAddress,
+    captcha: 'NO_CAPTCHA'
   });
 
   expect(res.status).toBe(200);
   expect(res.data.txHash).toBeTruthy();
   expect(res.data.checksumed).toBeFalsy();
 });
+/*
 test('# dispense to a rns alias (TODO), should dispense and return 200', () => {
   expect(true).toBeTruthy();
 });
 test("# dispense to a invalid rns alias (TODO), shouldn't dispense and return 409", () => {
   expect(true).toBeTruthy();
 });
-test('# dispense right value, should be ' + VALUE_TO_DISPENSE, async () => {
-  const dispenseAddress = randomAddress(3);
-  const balance = await web3.eth.getBalance(dispenseAddress);
-  await axios.post(API_URL + '/dispense', {
-    dispenseAddress
-  });
-  const currentBalance = await web3.eth.getBalance(dispenseAddress);
-  const expectedBalance = incrementByValueToDispense(balance);
-
-  expect(Number(currentBalance)).toBe(expectedBalance);
-});
-
 */
 
-const resetFaucetHistory = () => {
-  //clock.tick(86400000)
-};
+test('# dispense right value, should be ' + VALUE_TO_DISPENSE, async () => {
+  const dispenseAddress = accounts[3];
+  const oldBalance = Number(await web3.eth.getBalance(dispenseAddress));
+
+  await axios.post(apiUrl, {
+    dispenseAddress,
+    captcha: 'NO_CAPTCHA'
+  });
+
+  const currentBalance = Number(web3.utils.fromWei(await web3.eth.getBalance(dispenseAddress), 'ether'));
+  const expectedValueToDispense = currentBalance - oldBalance;
+
+  expect(expectedValueToDispense).toBe(valueToDispense());
+});
 
 const setupRNS = () => {};
-
-const incrementByValueToDispense = (balance: string) =>
-  Number(balance) + Number(web3.utils.toWei(valueToDispense().toString(), 'ether'));
