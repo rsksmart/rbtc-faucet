ce0012fd365c4f4835508e0d0d7b1aff14228b49
Fede Jinich <federico@iovlabs.org>
2019-11-07 18:51:10 -0300

    :sparkles: feature-card supports title and children

--- components/feature-card.tsx
+++ components/feature-card.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../assets/styles/sb-admin-2/sb-admin-2.scss'
 
-const FeatureCard = () => {
+interface FaucetCardProps {
+    title: string;
+    children: any;
+}
+
+const FeatureCard = (props: FaucetCardProps) => {
     return (
         <div className="col-xl-3 col-md-6 mb-4">
             <div className="card border-left-primary shadow h-100 py-2">
             <div className="card-body">
                 <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
-                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                    <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">{props.title}</div>
+                    <div className="h5 mb-0 font-weight-bold text-gray-800">{props.children}</div>
                 </div>
                 <div className="col-auto">
                     <i className="fas fa-calendar fa-2x text-gray-300"></i>
