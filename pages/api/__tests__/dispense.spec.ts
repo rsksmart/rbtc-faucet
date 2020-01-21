dc03d1be93817b0a577f9b058adac591f19844ed
Fede Jinich <federico@iovlabs.org>
2020-01-21 11:38:46 -0300

    :white_check_mark: removing unnecesary test

--- pages/api/__tests__/dispense.spec.ts
+++ pages/api/__tests__/dispense.spec.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import Web3 from 'web3';
 import http from 'http';
 import listen from 'test-listen';
+import handleDispense from '../dispense';
+import nock from 'nock';
 import { apiResolver } from 'next-server/dist/server/api-utils';
 import { captchaApiUrl, valueToDispense, provider } from '../../../utils/env-util';
-import handleDispense from '../dispense';
-import { isValidAddress, isValidChecksumAddress } from 'rskjs-util';
-import nock from 'nock';
+import { isValidChecksumAddress } from 'rskjs-util';
 
 const CAPTCHA_API_URL = captchaApiUrl();
 const VALUE_TO_DISPENSE = valueToDispense();
@@ -31,27 +31,17 @@ beforeAll(async () => {
 
   server = http.createServer(requestHandler);
   apiUrl = await listen(server);
-
-  setupRNS();
 });
 
 afterAll(async () => {
   server.close();
 });
 
-describe('NO CAPTCHA', () => {
+describe('MOCKED CAPTCHA', () => {
   beforeEach(() => {
     nock(CAPTCHA_API_URL) //Mocking captcha service
       .post('/solution/1/1')
       .reply(200, { result: 'accepted', reject_reason: '', trials_left: 5 });
-  });
-  test('there are five accounts', () => {
-    expect.assertions(2);
-
-    const validAddresses = accounts.map(e => isValidAddress(e));
-
-    expect(accounts.length).toBe(5);
-    expect(validAddresses.every(e => e)).toBeTruthy();
   });
   test('dispense to a new address', async () => {
     expect.assertions(3);
@@ -63,7 +53,7 @@ describe('NO CAPTCHA', () => {
     expect(response.data.txHash).toBeTruthy();
     expect(balance).toBe(valueToDispense());
   });
-  test('# dispense more than one time to an address, should only dispense the first time and then deny', async () => {
+  test('dispense more than one time to an address, should only dispense the first time and then deny', async () => {
     expect.assertions(5);
 
     const dispenseAddress = accounts[2];
@@ -99,7 +89,7 @@ describe('NO CAPTCHA', () => {
 
     expect(currentBalance).toBe(expectedBalance);
   });
-  test("# dispense to an invalid address, shouldn't dispense and respond 409", async () => {
+  test("dispense to an invalid address, shouldn't dispense and respond 409", async () => {
     expect.assertions(2);
 
     const oldFaucetTransactionCount = await web3.eth.getTransactionCount(faucetAddress);
@@ -115,7 +105,7 @@ describe('NO CAPTCHA', () => {
       expect(newFaucetTransactionCount).toBe(oldFaucetTransactionCount);
     }
   });
-  test('# dispense to an invalid checksum address, should dispense and respond 200', async () => {
+  test('dispense to an invalid checksum address, should dispense and respond 200', async () => {
     expect.assertions(4);
 
     const unchecksumedAddress: string = '0xD5e6Bfc7E5d982C1F7bAc4D9c7e769BEb0A6f626';
@@ -131,7 +121,9 @@ describe('NO CAPTCHA', () => {
     expect(res.data.txHash).toBeTruthy();
     expect(res.data.checksumed).toBeFalsy();
   });
-  test('# dispense right value, should be ' + VALUE_TO_DISPENSE, async () => {
+  test('dispense right value, should be ' + VALUE_TO_DISPENSE, async () => {
+    expect.assertions(1);
+
     const dispenseAddress = accounts[3];
     const oldBalance = Number(await web3.eth.getBalance(dispenseAddress));
 
