78c8f5d222650c243048c0b667fddbde3412c26f
Carlos Rosado <carlos.rosado@ext.iovlabs.org>
2024-01-23 15:27:14 -0600

    fix: getting captcha data when it generates an error

--- utils/validations.ts
+++ utils/validations.ts
@@ -1,12 +1,17 @@
 import { CaptchaSolutionResponse } from '../types/types';
 import {isValidAddress} from 'rskjs-util';
 
+interface ICaptchaResponse {
+  solution: string,
+  trials_left: number
+}
+
 export const insuficientFunds = (faucetBalance: number) =>
   faucetBalance < 100000000000000000 ? 'Faucet has not enough funds.' : '';
 export const needsCaptchaReset = (captchaSolutionResponse: CaptchaSolutionResponse): boolean =>
   captchaSolutionResponse.trials_left == 0;
-export const captchaRejected = (result: string): string =>
-  result == 'rejected' ? 'Invalid captcha (notice that this captcha is case sensitive).' : '';
+export const captchaRejected = (result: ICaptchaResponse): string =>
+  result.solution === 'accepted' ? '' : 'Invalid captcha (notice that this captcha is case sensitive).';
 export const alreadyDispensed = (dispenseAddress: string, faucetHistory: any): string =>
   faucetHistory.hasOwnProperty(dispenseAddress.toLowerCase()) ? 'Address already used today, try again tomorrow.' : '';
 export const invalidAddress = (dispenseAddress: string): string => !isValidAddress(dispenseAddress) ? 'Invalid address, provide a valid one.' : '';
