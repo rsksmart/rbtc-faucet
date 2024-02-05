caeeae5a82c132cb842099f1d266cacdef88dc97
Carlos Rosado <carlos.rosado@ext.iovlabs.org>
2024-02-05 10:33:06 -0600

    fix: changing the IOV logo to the rootstock logo

--- components/faucet.tsx
+++ components/faucet.tsx
@@ -43,12 +43,16 @@ const Faucet = (props: FaucetProps) => {
           onChange={props.onAddressChange}
         />
         <div className='captcha-content'>
-          <ReCAPTCHA
-            ref={props.captchaValue}
-            sitekey={props.siteKeyCaptcha}
-            theme='dark'
-            className={`re-captcha ${error.captchaValue ? 'error' : '' }`}
-          />
+          { props.siteKeyCaptcha ?
+            <ReCAPTCHA
+              ref={props.captchaValue}
+              sitekey={props.siteKeyCaptcha}
+              theme='dark'
+              className={`re-captcha ${error.captchaValue ? 'error' : '' }`}
+            />
+            :
+            <span className='spinner'></span>
+          }
         </div>
         <div className='content-btn'>
           <button onClick={handleForm} className="btn btn-primary btn-middle">
--- components/footer.tsx
+++ components/footer.tsx
@@ -6,12 +6,12 @@ function Footer() {
       <div className='copyright'>
         <div>
           <span>Built by</span>
-          <img src={require('../assets/images/iov-icon.svg')} alt="" />
+          <img src={require('../assets/images/rootstock-logo.svg')} alt="" />
         </div>
         <p>Copyright © 2015 RSK Labs. All rights reserved.</p>
       </div>
       <div className='links'>
-        <a href="https://www.iovlabs.org" target='_blank'>About IOV Labs</a>
+        <a href="https://rootstock.io/" target='_blank'>About Rootstock Labs</a>
         <a href="https://rootstock.io/community" target='_blank'>Help</a>
         <a href="https://rootstock.io/terms-conditions" target='_blank'>Terms & Conditions</a>
         <a href="https://dev.rootstock.io" target='_blank'>Documentation</a>
