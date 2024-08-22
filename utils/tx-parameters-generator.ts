446b93dbdc2c848be455da242d1e63c200375bf9
Carlos Rosado <carlos.rosado@ext.iovlabs.org>
2024-08-22 08:32:10 -0600

    feat: estimationFee function added

--- utils/tx-parameters-generator.ts
+++ utils/tx-parameters-generator.ts
@@ -4,14 +4,20 @@ import { faucetAddress } from './faucet-sensitive-util';
 import Web3 from 'web3';
 
 class TxParametersGenerator {
-  async generate(dispenseAddress: string, web3: Web3) {
+  async generate(dispenseAddress: string, web3: Web3, fee: number) {
+    const VALUE_TO_DISPENSE = valueToDispense();
+
+    const valueInWei = web3.utils.toWei(VALUE_TO_DISPENSE.toString(), 'ether');
+    const feeInWei = web3.utils.toWei(fee.toString(), 'ether');
+    const valueToSend = web3.utils.toBN(valueInWei).sub(web3.utils.toBN(feeInWei));
+
     const txParameters: TxParameters = {
       from: faucetAddress(),
       to: dispenseAddress,
       nonce: web3.utils.toHex(await web3.eth.getTransactionCount(faucetAddress(), 'pending')),
       gasPrice: web3.utils.toHex(gasPrice()),
       gas: web3.utils.toHex(gasLimit()),
-      value: web3.utils.toHex(web3.utils.toWei(valueToDispense().toString()))
+      value: web3.utils.toHex(valueToSend)
     };
 
     return txParameters;
