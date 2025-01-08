96cb19845ae568ae40919248c9bc61d45aee7e7a
Sebastian Guaqueta <scguaquetam@gmail.com>
2025-01-08 11:49:49 -0500

    clean not found component

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import NotFoundContent from '@/components/notFound'
-
-export default function NotFound() {
-  return (
-    <div className="body-faucet">
-      <NotFoundContent />
-    </div>
-  )
-}
