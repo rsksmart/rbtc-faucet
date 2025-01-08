c52cb7af8b2d6bada5a842a8a0e0d8b054f4cff6
Sebastian Guaqueta <scguaquetam@gmail.com>
2025-01-08 09:14:32 -0500

    creating not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import NotFoundContent from '@/components/notFound'
+
+export default function NotFound() {
+  return (
+    <div className="body-faucet">
+      <NotFoundContent />
+    </div>
+  )
+}
